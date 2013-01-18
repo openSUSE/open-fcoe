ab381fd3d8aa9b12626fad839a1df47aa170189d
Hannes Reinecke <hare@suse.de>
2013-01-18 12:19:00 +0100

    Add rpm build files
    Ported from SLES11 SP2.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

---
+++ rpm/mkinitrd-boot.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#%stage: device
+#%depends: network lldpad
+#%programs: /usr/sbin/fipvlan /usr/sbin/fcoeadm
+#%modules: $fcoe_drv 8021q
+#%if: "$root_fcoe"
+#
+##### FCoE initialization
+##
+## This script initializes FCoE (FC over Ethernet).
+
+load_modules
+
+lookup_vlan_if()
+{
+    local ifname=$1
+    local vlan vid if
+
+    IFS="| "; while read vlan vid if; do
+	[ "${vlan%% Dev*}" = "VLAN" ] && continue
+	[ -z "$vid" ] && continue
+	if [ "$if" = "$ifname" ] ; then
+	    echo $vlan
+	fi
+    done < /proc/net/vlan/config
+}
+
+lookup_fcoe_host()
+{
+    local ifname=$1
+    local h
+
+    for h in /sys/class/fc_host/host* ; do
+	[ -d "$h" ] || continue
+	[ -e $h/symbolic_name ] || continue
+	vif=$(sed -n 's/.* over \(.*\)/\1/p' $h/symbolic_name)
+	if [ "$vif" ] && [ "$ifname" = "$vif" ] ; then
+	    echo ${h##*/}
+	    break
+	fi
+    done
+}
+
+wait_for_fcoe_if()
+{
+    local if_list=$1
+    local host vif
+    local retry_count=$udev_timeout
+    local retry
+
+    vif_list=$(/usr/sbin/fipvlan --start --create $if_list | sed -n 's/\(eth[0-9]*\) *| \([0-9]*\) *|.*/\1.\2/p')
+    echo -n "Waiting for FCoE on $vif_list: "
+    while [ $retry_count -gt 0 ] ; do
+	retry=0
+	for vif in $vif_list ; do
+	    if ! ip link show $vif > /dev/null 2>&1 ; then
+		echo -n "O"
+		retry=$(($retry + 1));
+		continue;
+	    fi
+	    host=$(lookup_fcoe_host $vif)
+	    if [ -z "$host" ] ; then
+		echo -n "o"
+		retry=$(($retry + 1));
+		continue;
+	    fi
+	    status=$(cat /sys/class/fc_host/$host/port_state 2> /dev/null)
+	    if [ "$status" = "Online" ] ; then
+		continue;
+	    fi
+	    echo -n "."
+	    retry=$(($retry + 1));
+	done
+	[ $retry -eq 0 ] && break;
+        retry_count=$(($retry_count-1))
+        sleep 2
+    done
+    if [ $retry_count -eq 0 -a $retry -gt 0 ] ; then
+	echo "timeout; dropping to /bin/sh"
+	cd /
+	PATH=$PATH PS1='$ ' /bin/sh -i
+    else
+	echo "Ok"
+    fi
+}
+
+wait_for_fcoe_if "$fcoe_if $edd_if"
---
+++ rpm/mkinitrd-setup.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#
+#%stage: device
+#
+
+get_fc_host() {
+    local devname=${1##/dev/}
+    local sysfs_path
+
+    if [ -d /sys/block/$devname/device ] ; then
+	sysfs_path=$(cd -P /sys/block/$devname/device 2> /dev/null; echo $PWD)
+    fi
+    if [ -z "$sysfs_path" ] ; then
+	return;
+    fi
+
+    case "$sysfs_path" in
+        *rport-*)
+                shost_path=${sysfs_path%%/rport-*}
+                shost=${shost_path##*/}
+                ;;
+    esac
+
+    if [ -n "$shost_path" ] && [ -d "${shost_path}/fc_host/$shost" ] ; then
+	echo "$shost_path"
+    fi
+}
+
+get_fcoe_drvname() {
+    local shost_path=$1
+    local shost=${shost_path##*/}
+    local sysfs_path
+
+    fcoe_name=$(cat $shost_path/fc_host/$shost/symbolic_name)
+    case $fcoe_name in
+	*over*)
+	    echo "$fcoe_name"
+	    ;;
+    esac
+}
+
+for bd in $blockdev; do
+    update_blockdev $bd
+    shost_path="$(get_fc_host $bd)"
+    if [ "$shost_path" ]; then
+	symname="$(get_fcoe_drvname $shost_path)"
+	ifname=${symname#* over }
+	cur_drv=${symname%% *}
+	found=0
+	for d in $fcoe_drv ; do
+	    [ "$d" = "$cur_drv" ] && found=1
+	done
+	if [ "$found" = "0" ] ; then
+	    fcoe_drv="$fcoe_drv $cur_drv"
+	fi
+	if [ -f /proc/net/vlan/$ifname ] ; then
+	    cur_if=$(sed -n 's/Device: \(.*\)/\1/p' /proc/net/vlan/$ifname)
+	else
+	    cur_if=$ifname
+	fi
+	found=0
+	for i in $fcoe_if ; do
+	    [ "$i" = "$cur_if" ] && found=1
+	done
+	if [ "$found" = "0" ] ; then
+	    fcoe_if="$fcoe_if $cur_if"
+	fi
+	pci_path=${shost_path%/*}
+	case "$pci_path" in
+	    *virtual*)
+		pci_path="/sys/class/net/$cur_if/device"
+		;;
+	esac
+	pci_drv=$(readlink $pci_path/driver)
+	for d in $drvlink ; do
+	    [ "$d" = "$pci_drv" ] && found=1
+	done
+	if [ "$found" = 0 ] ; then
+	    drvlink="$drvlink ${pci_drv##*/}"
+	fi
+    	root_fcoe=1
+    fi
+done
+
+save_var root_fcoe
+save_var fcoe_if
+save_var fcoe_drv
+save_var drvlink
+
+if [ "${root_fcoe}" ] ; then
+    # Create /usr/sbin directory if not present
+    [ -d $tmp_mnt/usr/sbin ] || mkdir -p $tmp_mnt/usr/sbin
+    cp /etc/hba.conf ${tmp_mnt}/etc
+    libhbalinux=$(sed -n 's/org.open-fcoe.libhbalinux *\(.*\)/\1/p' /etc/hba.conf)
+    if [ "$libhbalinux" ] ; then
+	cp $libhbalinux ${tmp_mnt}$libhbalinux
+    fi
+    if [ -f "/etc/fcoe/cfg-${fcoe_vif}" ] ; then
+        # copy the fcoe configuration
+	mkdir $tmp_mnt/etc/fcoe
+	cp /etc/fcoe/cfg-${fcoe_vif} ${tmp_mnt}/etc/fcoe
+    fi
+fi
